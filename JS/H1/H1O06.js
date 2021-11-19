de3c7207a7e2e17545915bb5f46f230bfaf7cd49
esthertje <153003@csgaugustinus.nl>
2021-11-19 13:11:26 +0000

    ok

--- JS/H1/H1O06.js
+++ JS/H1/H1O06.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   noStroke();
-  fill('mediumaquamarine');
+  fill('thistle');
   translate(50,50);
   rect(0,0,100,100);
   
@@ -15,19 +15,20 @@ function draw() {
   rect(0,0,100,100);
   translate(140,0);
   rect(0,0,100,100);
-  
   translate(140,0);
-  // BEGIN aanpassing
+  push();
   
   stroke('cornsilk');
   strokeWeight(20);
   fill('teal');
   translate(0,40);  
   rect(0,0,100,100);  
-  // EINDE aanpassing
+  
   
   translate(140,0);
   rect(0,0,100,100);
+  pop();
   translate(140,0);
   rect(0,0,100,100);  
+  
 }
