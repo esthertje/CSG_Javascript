17a408b055dc7dc2c68a27de3893d359875af1f0
esthertje <153003@csgaugustinus.nl>
2021-11-23 13:19:16 +0000

    ok

--- JS/H1/H1O13.js
+++ JS/H1/H1O13.js
@@ -14,7 +14,8 @@ function draw() {
   text("xJOS: " + round(xJOS) + " (mouseX:" + round(mouseX) + ")",10,20);
   text("yJOS: " + round(yJOS) + " (mouseY:" + round(mouseY) + ")",260,20);
   
-  xJOS = constrain(mouseX,100,450);
+  xJOS = constrain(mouseX,75,375);
+  yJOS = constrain(mouseY,75,375);
   
   scale(1);
   translate(xJOS,225);
